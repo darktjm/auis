cc8479a20da5a5bcd64fc4c9e07473a4d5e5fb48
darktjm <darktjm@7c4214cd-b045-46d4-a95a-930570af3fd6>
2016-08-24 16:25:00 +0000

    Don't use temporary files for description insets in org. This makes the write output incompatible with old (but broken anyway) org, and only partly supports broken documents for reading.  I've corrected the one and only example I have of a broken document as well, by just loading and saving it.
    git-svn-id: svn://dlink/auis@382 7c4214cd-b045-46d4-a95a-930570af3fd6

--- atk/org/org.C
+++ atk/org/org.C
@@ -145,8 +145,7 @@ long Read_Body( register class org		      *self, register FILE			      *file )
   register long			      c, count, braces = 0, brackets = 0, status = ok,
 				      description_size, description_length,
 				      description_increment = 32;
-  char				      string[4096], *ptr, *end_ptr,
-				     *count_ptr, counter[32];
+  char				      string[4096], *ptr, *end_ptr;
   register char			     *description;
   register tree_type_node	      parent = NULL, child = NULL, node;
   class text			     *textp = NULL;
@@ -179,45 +178,32 @@ long Read_Body( register class org		      *self, register FILE			      *file )
 	if(ptr > string)
 	    break;
 	braces--;
-	if ( ptr > string ) {
-	  Strip( ptr = string );
-	  if ( parent ) {
-	    if ( (child = node = (Tree)->CreateChildNode(  string, 0, parent )) == NULL )
-	      { DEBUG(ERROR Creating Node);/*===*/      }
-	  }
-	  else {/*===*/}
-	}
-	else {
-	  child = parent;
-	  parent = (Tree)->ParentNode(  parent );
-	}
+	child = parent;
+	parent = (Tree)->ParentNode(  parent );
         continue;
       case '[':
 	if(ptr > string)
 	    break;
 	brackets++;
-	count_ptr = counter;
-	while ( (c = getc( file ))  &&  c != EOF  && c != '\n' )
-	  *count_ptr++ = c;
-	*count_ptr = 0;
-	count = atoi( counter );
-	DEBUGdt(Count,count);
-	description = (char *) malloc( count );
-	fread(description, count, 1, file);
-	{
-	  FILE *f = tmpfile();
-	  fwrite(description, count, 1, f);
-	  free(description);
-	  fputc('\n', f);
-	  rewind(f);
+	/* I'd like to ignore length, since inset reader will stop at end */
+	/* However, some files (e.g. example3.org) have broken insets */
+	/* As a compromise, if length=0, ignore length */
+	/* This will of course be broken on unseekables, but who cares? */
+	while((c = getc(file)) != '\n' && c != EOF) *ptr++ = c;
+	*ptr = 0;
+	ptr = string;
 	        {
+		    int len = atoi(string);
+		    unsigned long epos = 0;
+		    if(len > 0)
+			epos = ftell(file) + len;
 		    long objID;
-		    filetype::Lookup(f, NULL, &objID, NULL);
+		    filetype::Lookup(file, NULL, &objID, NULL);
 		    textp = new text;
-		    (textp)->Read( f, objID);
+		    (textp)->Read( file, objID);
+		    if(epos)
+			fseek(file, epos, SEEK_SET);
 		}
-	  fclose(f);
-	}
 	(Tree)->SetNodeDatum(  node, (long) textp );
 	continue;
       case ']':
@@ -225,10 +211,22 @@ long Read_Body( register class org		      *self, register FILE			      *file )
 	    break;
 	brackets--;
 	continue;
-      case '\\': /* no idea what old behavior was supposed to do */
+      case '\\':
+	/* the old code pretty much aborted no matter what, here */
+	/* I guess that meant it always assumed it was \enddata */
+	/* I prefer allowing backslashes within the node name */
 	if(ptr > string)
 	    break;
-	if ( (c = getc( file )) != '\n'  &&  c != EOF ) break;
+	/* and at the beginning, to escape \ { [ */
+	if ( (c = getc( file )) == '\\' || c == '[' || c == '{')
+	    break;
+	/* now it's either enddata or an error.  In any case, just
+	 * suck the line in and exit */
+	/* not that org is embeddable in other docs, anyway, but also
+	 * stop on }, and if not @ eol, preserve char after } */
+	while ( c != EOF && c != '}' && (c = getc( file )) != '\n' );
+	if(c == '}' && (c = getc( file )) != '\n')
+	    ungetc(c, file);
 	done = true;
 	continue;
       case EOF:
@@ -300,27 +298,17 @@ long Write_Body( register class org  *self, register FILE  *file, long writeID )
     fprintf( file, "%*s%s%s\n", (int)(2 * level), "", bs, name );
     if ( (textp = (class text *) (Tree)->NodeDatum( node)) && 
 	 (size = (textp)->GetLength()) > 0 ) {
-	long realSize = 0;
-	char *description;
-	FILE *f;
-	if(f = tmpfile()) {
-	    (textp)->Write( f, writeID, 1);
-	    fseek(f, 0, 2);
-	    realSize = ftell(f);
-	    rewind(f);
-	    {
-		if(description = (char*) malloc(realSize + 1)) {
-		    if(fread(description, realSize, 1, f) != 1) {
-			fprintf(stderr, "org: incomplete read on temp file\n");
-			realSize = 0;
-		    }
-		    description[realSize] = (char)0;
-		}
-	    }
-	    fclose(f);
-	}
-	fprintf( file, "%*s[%ld\n%s]\n", (int)(2 * level), "", realSize, description);
-	free(description);
+	/* don't write to tmp file, and don't output size of text */
+	/* it's unnecessary and just makes this code uglier than needed */
+	/* however, it does make this incompatible with official */
+	/* (and mostly broken anyway) org.  Oh well. */
+	/* I'll still write a length of 0 just for giggles.  The old */
+	/* org will abort reading shortly thereafter */
+	/* I'm tempted to just drpo the whole square bracket crap and use */
+	/* \begindata to signify descriptions */
+	fprintf( file, "%*s[0\n", (int)(2 * level), "" );
+	(textp)->Write( file, writeID, 1);
+        fputs( "]\n", file);
     }
     node = (Tree)->NextNode(  node );
   }
